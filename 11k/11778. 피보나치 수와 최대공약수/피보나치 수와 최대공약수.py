41c71151db2a24d07f705736ce95a89476612866
ered1228 <hwangyu1228@gmail.com>
2024-09-23 02:39:06 +0900

    Platinum V

---
+++ 11k/11778. 피보나치 수와 최대공약수/피보나치 수와 최대공약수.py
@@ -0,0 +1,30 @@
+import math
+
+def matrix_multi_2by2(A, B):
+    Mat = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                Mat[i][j] += A[i][k] * B[k][j]
+                Mat[i][j] %= 10**9 + 7
+    return Mat
+
+def matrix_pow(Mat, n):
+    I = [[1, 0], [0, 1]]
+    base = Mat
+    while n > 0:
+        if n % 2 == 1:
+            I = matrix_multi_2by2(I, base)
+        base = matrix_multi_2by2(base, base)
+        n //= 2
+    return I
+
+Mat = [[1, 1], [1, 0]]
+MOD = 10**9 + 7
+n, m = map(int, input().split())
+g = math.gcd(n, m)
+if g == 1:
+    print(1)
+else:
+    fn = matrix_pow(Mat, g-1)[0][0]
+    print(fn % MOD)
