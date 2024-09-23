857b6cebec5c562153ec26388feab6e2f876e63b
Hwangyu Kim <hwangyu1228@gmail.com>
2024-09-23 17:41:00 +0900

    Silver IV

--- 32k/32328. Bronze Count/Bronze Count.py
+++ 32k/32328. Bronze Count/Bronze Count.py
@@ -1,18 +1,21 @@
+import sys
 from bisect import bisect_left, bisect_right
+input = sys.stdin.readline
 
 n = int(input())
-score = [0]*n
-
-for i in range(n):
+S = [0]*76
+for _ in range(n):
     a = int(input())
-    score[i] = a
+    S[a] += 1
 
-scoreset = list(set(score))
-scoreset.sort()
-scoreset.remove(scoreset[-1])
-scoreset.remove(scoreset[-1])
-bronze = max(scoreset)
-
-score.sort()
-cnt = bisect_right(score, bronze) - bisect_left(score, bronze)
-print(bronze, cnt)
+b = 0
+c = 0
+cnt = 0
+for i in range(75, -1, -1):
+    if S[i] > 0:
+        c += 1
+    if c == 3:
+        b = i
+        cnt = S[i]
+        break
+print(b, cnt)
