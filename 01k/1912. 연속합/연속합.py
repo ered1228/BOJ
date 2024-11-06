7c59a81d2548dff1663c9de7b40d4db1bfb8b6f2
Hwangyu Kim <hwangyu1228@gmail.com>
2024-11-06 21:56:14 +0900

    Silver II

--- 01k/1912. 연속합/연속합.py
+++ 01k/1912. 연속합/연속합.py
@@ -1,7 +1,12 @@
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [arr[0]]
-res = arr[0]
+
+if arr[0] >= 0:
+    dp = [arr[0]]
+    res = arr[0]
+else:
+    dp = [0]
+    res = 0
 
 for i in range(1, n):
     dp.append(dp[i-1] + arr[i])
