6a958c24c95f662197fcc12d13c2e6e0aab503d0
Hwangyu Kim <hwangyu1228@gmail.com>
2024-11-06 20:36:05 +0900

    Silver II

---
+++ 24k/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def DFS(graph, start):
+    visited = set()
+    order = []
+    stack = [start]
+    while stack:
+        temp = stack.pop()
+        if temp not in visited:
+            visited.add(temp)
+            order.append(temp)
+            S = set(graph[temp]) - visited
+            for s in S:
+                stack.append(s)
+    return order
+
+N, M, R = map(int, input().split())
+graph = {i:[] for i in range(1, N+1)}
+res = [0]*N
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for key in graph:
+    graph[key].sort()
+
+V = DFS(graph, R)
+for inx, v in enumerate(V, start=1):
+    res[v-1] = inx
+
+for r in res:
+    print(r)
