66a2d78ba138a5222aca28f4df2b9e313482f995
Hwangyu Kim <hwangyu1228@gmail.com>
2024-11-23 17:02:01 +0900

    Bronze I

---
+++ 04k/4150. 피보나치 수/피보나치 수.py
@@ -0,0 +1,23 @@
+def matrix_pow_2(A, B):
+    R = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                R[i][j] += A[i][k] * B[k][j]
+    return R
+
+def fibo(n):
+    I = [[1, 0], [0, 1]]
+    base = [[1, 1], [1, 0]]
+    while n > 0:
+        if n % 2 == 1:
+            I = matrix_pow_2(I, base)
+        base = matrix_pow_2(base, base)
+        n //= 2
+    return I
+
+n = int(input())
+if n <= 2:
+    print(1)
+else:
+    print(fibo(n-1)[0][0])
