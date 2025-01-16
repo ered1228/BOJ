2a4a91b713633ab6b6c6888c78a53f8cccae4f33
Hwangyu Kim <hwangyu1228@gmail.com>
2025-01-16 16:38:49 +0900

    Silver III

---
+++ 15k/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,19 @@
+def sol(temp, l):
+    if l == m:
+        print(' '.join(map(str, temp)))
+        return
+    else:
+        for i in range(n):
+            if J[i]:
+                if (not temp) or (temp[-1] < A[i]):
+                    temp.append(A[i])
+                    J[i] = False
+                    sol(temp, l+1)
+                    temp.pop()
+                    J[i] = True
+
+n, m = map(int, input().split())
+A = sorted(list(map(int, input().split())))
+J = [True]*n
+temp = []
+sol(temp, 0)
