ba0a6c9c41e4de0ff593f1c93a69bfa017a9f75f
Hwangyu Kim <hwangyu1228@gmail.com>
2025-02-02 18:32:00 +0900

    Bronze IV

---
+++ 33k/33191. Yalda/Yalda.py
@@ -0,0 +1,13 @@
+b = int(input())
+Watermelon = int(input())
+Pomegranates = int(input())
+Nuts = int(input())
+
+if Watermelon <= b:
+    print('Watermelon')
+elif Pomegranates <= b:
+    print('Pomegranates')
+elif Nuts <= b:
+    print('Nuts')
+else:
+    print('Nothing')
