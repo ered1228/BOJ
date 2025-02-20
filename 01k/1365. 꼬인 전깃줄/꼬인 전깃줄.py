36db810204c3d825f4f815ecf27202ef80af9d15
Hwangyu Kim <hwangyu1228@gmail.com>
2025-02-20 14:34:06 +0900

    Gold II

---
+++ 01k/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+
+n = int(input())
+S = [0] + list(map(int, input().split()))
+mv = [0]
+for i in range(1, n+1):
+    temp = S[i]
+    idx = bisect_left(mv, temp)
+    if idx == len(mv):
+        mv.append(temp)
+    else:
+        mv[idx] = temp
+print(n-len(mv)+1)
