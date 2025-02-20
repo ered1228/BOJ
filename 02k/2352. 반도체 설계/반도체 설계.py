9c601593f52ba6bdaa7c3c17a3e72801259a9052
Hwangyu Kim <hwangyu1228@gmail.com>
2025-02-20 14:32:36 +0900

    Gold II

---
+++ 02k/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,14 @@
+from bisect import bisect_left
+
+n = int(input())
+S = [0] + list(map(int, input().split()))
+mv = [0]
+
+for i in range(1, n+1):
+    temp = S[i]
+    idx = bisect_left(mv, temp)
+    if idx == len(mv):
+        mv.append(temp)
+    else:
+        mv[idx] = temp
+print(len(mv)-1)
